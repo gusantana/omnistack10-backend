6073eed18b0276fcf3968a195a52fcc07e6d90f4
Gustavo Santana Coelho <gustavo_13roo@hotmail.com>
2020-03-23 19:33:21 -0400

    primeiro commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const mongoose = require('mongoose');
+const routes = require('./routes')
+const cors = require('cors');
+
+const app = express();
+
+mongoose.connect('mongodb+srv://gustavo:mjpq282@lotus-x5ec7.mongodb.net/week10?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+app.listen(3333);
